0d654253478771e9b1451f7caaf3759a22ed4dba
Ladislav Slezák <lslezak@suse.cz>
2014-01-09 11:52:28 +0100

    generate *-promo files in the "tarball" rake task

--- Rakefile
+++ Rakefile
@@ -5,3 +5,13 @@ Yast::Tasks.configuration do |conf|
   conf.skip_license_check << /.*/
 end
 
+desc "Generate *-promo package files"
+task :create_promo do
+  Dir.chdir("package") do
+    sh "./pre_checkin.sh"
+  end
+end
+
+# generate the *-promo files when creating the tarball
+task :tarball => :create_promo
+
--- package/pre_checkin.sh
+++ package/pre_checkin.sh
@@ -3,5 +3,8 @@
 cp skelcd-control-openSUSE.changes  skelcd-control-openSUSE-promo.changes
 sed -e 's,^Name.*,Name: skelcd-control-openSUSE-promo,' skelcd-control-openSUSE.spec > skelcd-control-openSUSE-promo.spec
 
-osc service localrun format_spec_file
+# this does not work in the Git checkout, osc checkout is needed
+if [ -e .osc ]; then
+  osc service localrun format_spec_file
+fi
 
